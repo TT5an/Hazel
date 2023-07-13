131c42e4e898da61d0efe927b70d4dfb4bb53d3f
TT5an <1319250858@qq.com>
2023-07-13 12:22:20 +0800

    创建项目，修改项目设置，创建进入点

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+// 为Hazel引擎程序提供
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+//---------进入点-----------------
+#include "Hazel/EntryPoint.h"
+//-------------------------------
